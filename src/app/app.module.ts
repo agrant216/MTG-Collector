2dd06b8fecd9853a6928bc83c7bfc8f0aa02df28
agrant216 <agrant12@kent.edu>
2019-09-26 19:15:33 +0000

    initial commit - getting the pieces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollectorModule } from './features/collector/collector.module';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SideNavComponent } from './component/side-nav/side-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTreeModule } from '@angular/material/tree';
+import { CollectorComponent } from './features/collector/collector.component';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SideNavComponent,
+    CollectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CollectorModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
